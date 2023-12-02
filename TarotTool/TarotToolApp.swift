019c9381ddaaa1c2e6b1a382eb20faff197ceb62
Halcyone Rapp <halcyone.rapp@gmail.com>
2023-12-01 20:10:12 -0800

    Initial Commit

---
+++ TarotTool/TarotToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  TarotToolApp.swift
+//  TarotTool
+//
+//  Created by Halcyone Rapp on 12/1/23.
+//
+
+import SwiftUI
+
+@main
+struct TarotToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
